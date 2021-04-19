84fd354756b80d81db1e1454a1d9d8c360bd52a9
Yao Chi <yaochi@hnrfqj.com>
2021-04-19 17:11:16 +0800

    添加头部栏

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,17 +1,22 @@
 module.exports = {
-  base: '/vuepress-starter/',
-  dest: 'dist',
-  title: 'TypeScript 入门',
-  description: '学习使用 TypeScript',
+  head: [["link", { rel: "icon", href: "/ts-logo.jpg" }]],
+  base: "/vuepress-starter/",
+  dest: "dist",
+  title: "TypeScript 入门",
+  description: "学习使用 TypeScript",
   themeConfig: {
     sidebar: [
       {
-        title: '初识 TypeScript',
+        title: "初识 TypeScript",
         collapsable: false,
-        children: [
-          'chapter1/01_初识TS'
-        ]
-      }
-    ]
-  }
-}
+        children: ["chapter1/01_初识TS"],
+      },
+    ],
+    nav: [
+      {
+        text: "TypeScript 笔记",
+        link: "/chapter1/01_初识TS",
+      },
+    ],
+  },
+};
